23ef18e339662793e0b0206b346e63b4bb7b6784
Quinton Roberts <quinton@qjr.ca>
2014-07-07 19:03:23 -0400

    Add startup script

---
+++ startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get currect directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+HOME_FILES=( ".gitconfig" ".zshrc" ".vimrc" ".vim" )
+
+for f in "${HOME_FILES[@]}"
+do
+  TARGET="$HOME/$f"
+
+  if [ -e "$TARGET" ]; then
+    rm -r $TARGET
+  fi
+
+  ln -s "$DIR/$f" $HOME
+done
+
